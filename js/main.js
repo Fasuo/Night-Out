286c92f0944482a53f47147361e7b792e4a36c32
RaunoL <raunolillemets@hotmail.com>
2019-03-03 16:29:43 +0200

    added venue review and recommendations

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ document.getElementById("login").addEventListener("click", login);
 document.getElementById("register").addEventListener("click", register);
 document.getElementById("search").addEventListener("click", search);
 document.getElementById("addNewVenue").addEventListener("click", addNewVenue);
+document.getElementById("currentlySelectedRecommend").addEventListener("click", recommendVenue);
+document.getElementById("currentlySelectedReview").addEventListener("click", reviewVenue);
 function sessionStart(){
     $.ajax({
         url: "php/startSession.php",
@@ -9,6 +11,8 @@ function sessionStart(){
     });
 }
 sessionStart();
+
+
 function login(){
     let username= document.getElementById("username").value;
     let password= document.getElementById("password").value;
@@ -57,8 +61,11 @@ function search(){
             type: type
         },
         success: function (data) {
-            console.log(data);
-            document.getElementById('searchResults').innerHTML+= data;
+
+
+            searchResults(data);
+            // document.getElementById('searchResults').innerHTML+= data;
+
         },
         error: function (data) {
             console.log(data)
@@ -80,6 +87,96 @@ function addNewVenue(){
         success: function (data) {
             console.log(data);
 
+        },
+        error: function (data) {
+            console.log(data)
+        }
+    });
+}
+function searchResults(data){
+    var venues= data.split(",");
+    venues.pop();
+    var i=0;
+    while (i<venues.length){
+        var venue=venues[i].split(":");
+        var searchResult= "<li><a class=venue  id=venue" + venue[1] + ">"+venue[0]+"</a></li>";
+        document.getElementById('searchResults').innerHTML+= searchResult;
+        i++
+    }
+
+    document.querySelectorAll(".venue").forEach(function(elem) {
+        elem.addEventListener("click", function() {
+            venueDetails(this.id);
+            // console.log(this.id);
+        });
+    });
+}
+function venueDetails(id) {
+    let venueId= id.replace( /^\D+/g, '');
+    $.ajax({
+        url: "php/venueDetails.php",
+        type: "POST",
+        data: {
+            id: venueId
+        },
+        success: function (data) {
+            var currentlySelectedData=data.split("|")
+            var currentlySelected=currentlySelectedData[0].split("/");
+            var currentlySelectedReviews= currentlySelectedData[1].split("/");
+            currentlySelectedReviews.pop();
+            console.log(currentlySelectedData);
+            console.log(currentlySelected);
+            console.log(currentlySelectedReviews);
+            document.getElementById("currentlySelectedName").innerText= currentlySelected[0];
+            document.getElementById("currentlySelectedType").innerText= currentlySelected[1];
+            document.getElementById("currentlySelectedDesc").innerText= currentlySelected[2];
+            document.getElementById("currentlySelectedRecommendations").innerText= currentlySelected[3]+" Recommendations";
+            document.getElementById("currentlySelectedRecommend").value= "recommend"+venueId;
+            document.getElementById("currentlySelectedReviews").innerHTML= "";
+            var i =0;
+            while (i<currentlySelectedReviews.length){
+                document.getElementById("currentlySelectedReviews").innerHTML+= "<li>"+currentlySelectedReviews[i]+"";
+                i++
+            }
+            document.getElementById("currentlySelectedReview").value= "review"+venueId;
+            document.getElementById("currentlySelected").style.display="block";
+            console.log(id);
+        },
+        error: function (data) {
+            console.log(data)
+        }
+    });
+}
+function recommendVenue() {
+    let venueId= document.getElementById("currentlySelectedRecommend").value.replace( /^\D+/g, '');
+    $.ajax({
+        url: "php/recommendVenue.php",
+        type: "POST",
+        data: {
+            id: venueId
+        },
+        success: function (data) {
+            console.log(data);
+            venueDetails(venueId)
+        },
+        error: function (data) {
+            console.log(data)
+        }
+    });
+}
+function reviewVenue() {
+    let venueId= document.getElementById("currentlySelectedReview").value.replace( /^\D+/g, '');
+    let venueReview= document.getElementById("venueReview").value;
+    $.ajax({
+        url: "php/reviewVenue.php",
+        type: "POST",
+        data: {
+            id: venueId,
+            review: venueReview
+        },
+        success: function (data) {
+            console.log(data);
+            venueDetails(venueId)
         },
         error: function (data) {
             console.log(data)
--- php/search.php
+++ php/search.php
@@ -7,13 +7,11 @@ $results = $conn->prepare("select * from venues where name like ? and type like 
 $results->bindParam(1,$keyword);
 $results->bindParam(2,$type);
 $results->execute();
-//$sql= "select * from venues where name like $keyword $type";
-//echo $sql;
-//$results = $conn->query($sql);
-$searchResults= array();
+
 
 while($row=$results->fetch())
 {
-    array_push($searchResults, $row["name"]);
-    echo "<h1>".$row["name"]."</h1>";
+
+    echo $row["name"].":".$row["ID"].",";
 }
+
