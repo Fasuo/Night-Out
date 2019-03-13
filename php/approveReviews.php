cd95ed74f3350a8d24bdfa66ea552e730eae2d13
RaunoL <raunolillemets@hotmail.com>
2019-03-13 13:28:14 +0200

    what

--- php/approveReviews.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-$id = $_POST["id"];
-$conn = new PDO("mysql:host=localhost;dbname=assign118;", "assign118","ao9ZeeJa");
-$results = $conn->prepare("UPDATE reviews SET approved = 1 WHERE ID=?;");
-$results->bindParam(1,$id);
-$results->execute();
