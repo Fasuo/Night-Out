2525de6dd3ab232b68e4ef5b220c19ab1a47c1e1
raunol <3lillr84@solent.ac.uk>
2019-03-13 13:27:22 +0200

    Index php and sepparation

--- php/login.php
+++ php/login.php
@@ -1,8 +1,7 @@
 <?php
-session_start();
 $un = $_POST["username"];
 $pw = $_POST["password"];
-$conn = new PDO("mysql:host=localhost;dbname=assign118;", "assign118","ao9ZeeJa");
+$conn = new PDO("mysql:host=localhost;dbname=assign118;", "root");
 
 $results = $conn->prepare("select * from users where name=?");
 $results->bindParam(1,$un);
@@ -12,21 +11,23 @@ if($row=$results->fetch())
     if ($row["password"]==$pw){
         $_SESSION["gatekeeper"] = $un;
         $_SESSION["isadmin"]= $row["isadmin"];
+        echo $_SESSION["gatekeeper"] ;
+        header('Location: ../index.php');
 
-        if ($_SESSION["isadmin"]==1){
-            $results = $conn->prepare("select * from reviews where approved=0");
-            $results->execute();
-            while($row=$results->fetch())
-            {
-//                echo $row["review"].$row["username"];
-                echo "<tr>";
-                echo "<td>".$row["review"]."</td>";
-                echo "<td>".$row["username"]."</td>";
-                echo "<td><button class=approveReview id=approve".$row["ID"].">Approve</button></td>";
-                echo "</tr>";
-
-            }
-        }
+//        if ($_SESSION["isadmin"]==1){
+//            $results = $conn->prepare("select * from reviews where approved=0");
+//            $results->execute();
+//            while($row=$results->fetch())
+//            {
+////                echo $row["review"].$row["username"];
+//                echo "<tr>";
+//                echo "<td>".$row["review"]."</td>";
+//                echo "<td>".$row["username"]."</td>";
+//                echo "<td><button class=approveReview id=approve".$row["ID"].">Approve</button></td>";
+//                echo "</tr>";
+//
+//            }
+//        }
     }
     else{
         echo "wrong password";
--- php/register.php
+++ php/register.php
@@ -2,7 +2,7 @@
 session_start();
 $un = $_POST["username"];
 $pw = $_POST["password"];
-$conn = new PDO("mysql:host=localhost;dbname=assign118;", "assign118","ao9ZeeJa");
+$conn = new PDO("mysql:host=localhost;dbname=assign118;", $dbname);
 
 
 $results = $conn->prepare("INSERT INTO users(name,password, isadmin) VALUES (?,?,0)");
